b7663752fa5147325e292397d563473c82e661e5
Wilmer Arenas <wilmerarenas24@gmail.com>
2024-02-29 19:53:26 -0500

    Implementando el test para validar que la ruta que tare los usuarios, traiga un array

--- tests/test.js
+++ tests/test.js
@@ -8,5 +8,23 @@ describe('GET /', () => {
         const response = await request(app).get('/');
         expect(response.status).toBe(200);
         expect(response.text).toBe('Hello world'); // Corregí "word" a "world"
-    });
+    })
+
+    it('responds with an array object that contains an specific user', async () => { // Cambié "asyn" a "async"
+        const response = await request(app).get('/user');
+        
+        const objectToTest = {
+            "_id": "65cfe1fe297a14a9e28374a0",
+            "id": 12,
+            "name": "prueba correo actualizacion 3",
+            "lastname": "Prueba 3",
+            "email": "Pruebaactualizacion@gmail.com",
+            "password": "$2b$10$SoXw71E6nElIj.Hc2vOjS.ndM14coogxGdvfPo.YrVQXQZhhwhcSe",
+            "__v": 0,
+            "avatar": "uploads\\1708390891252-gato.png"
+        };
+        
+        expect(Array.isArray(response.body)).toBe(true);
+        expect(response.body).toEqual(expect.arrayContaining([objectToTest])); 
+    })
 });
