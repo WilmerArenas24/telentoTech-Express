04c7fac180a5a64705e0a0e20a7048ca4d0a85f0
Wilmer Arenas <wilmerarenas24@gmail.com>
2024-03-04 20:44:30 -0500

    realizando la implementacion de ETL para subir usuarios desde un archivo xlsx a BD

--- upload_data.js
+++ upload_data.js
@@ -1,7 +1,22 @@
 const xlsx = require('xlsx')
+require('dotenv').config() // Obetenmos las variables de entorno
+const bcrypt = require('bcrypt');
+
+/** Conexion a BD */
+const DB_URL = process.env.DB_URL || '';
+const mongoose = require('mongoose'); // Importo la libreria mongoose
+mongoose.connect(DB_URL) // Creo la cadena de conexion
+const UserSchema = require('./models/User') 
 
 const workbook = xlsx.readFile('datos.xlsx')
 const sheet_list = workbook.SheetNames
 const data = xlsx.utils.sheet_to_json(workbook.Sheets[sheet_list[0]])
 
-console.log(data)
+for(const user of data){
+    const hashedPassword = bcrypt.hashSync(user.password, 10)
+    user.password = hashedPassword
+}
+
+UserSchema.insertMany(data).then(()=>{
+    console.log('Información subida exitosamennte')
+}).catch(err => console.log('Error subiendo la información', err))
